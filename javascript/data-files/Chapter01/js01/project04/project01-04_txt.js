057a34ce44e7e39378e2f8a5a8a9a08e8ea62250
jorge-mejia13 <jorge_mejia@insideranken.org>
2025-10-25 19:08:56 -0500

    JavaScript CH01 Hands-On Projects

--- javascript/data-files/Chapter01/js01/project04/project01-04_txt.js
+++ javascript/data-files/Chapter01/js01/project04/project01-04_txt.js
@@ -2,11 +2,18 @@
     Chapter 1
     Hands-On Project 1-4
 
-    Author: 
-    Date:   
+    Author: Jorge Mejia
+    Date: 10/25/25
 
     Filename: project01-04.js
 */
 
 //define variables for home and work addresses
-
+let homeStreet = '1 Main St.'
+let homeCity = 'Sicilia'
+let homeState = 'MA'
+let homeCode = '02103'
+let workStreet = '15 Oak Ln.'
+let workCity = 'Central City'
+let workState = 'MA'
+let workCode = '02104'
