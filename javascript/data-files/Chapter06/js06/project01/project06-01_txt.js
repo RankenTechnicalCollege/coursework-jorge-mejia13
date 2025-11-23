b5b2b1658ac2a5715ead2ef90038daacca0aa2bf
jorge-mejia13 <jorge_mejia@insideranken.org>
2025-11-23 15:25:29 -0600

    CH6 Project 1 Started

--- javascript/data-files/Chapter06/js06/project01/project06-01_txt.js
+++ javascript/data-files/Chapter06/js06/project01/project06-01_txt.js
@@ -4,9 +4,22 @@
       Project 06-01
 
       Project to validate a form used for setting up a new account
-      Author: 
-      Date:   
+      Author: jorge mejia
+      Date: 11/16/25
 
       Filename: project06-01.js
 */
 
+let submitButton = document.getElementById("submitButton");
+let pwd = document.getElementById('pwd');
+let pwd2 = document.getElementById('pwd2')
+
+submitButton.addEventListener('click', function() {
+      if (pwd.checkValidity()) {
+            pwd.setCustomValidity("Your password must be at least 8 characters with at least one letter and one number")
+      } else if (pwd.value !== pwd2.value) {
+            pwd2.setCustomValidity('Your passwords must match')
+      } else {
+            alert('')
+      }
+});
